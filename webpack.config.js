5aed8cd33a00ce3192b73daa00c5a7747216d21d
Andrew Kwok <67013831+aw-kwok@users.noreply.github.com>
2024-07-30 00:11:50 -0400

    Added support for package handling with Webpack, restructured directory

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: {
+        background: './src/background.js',
+        content: './src/content.js',
+        popup: './src/popup.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/popup.html', to: 'popup.html' },
+                { from: 'src/styles.css', to: 'styles.css' }
+            ]
+        })
+    ]
+};
